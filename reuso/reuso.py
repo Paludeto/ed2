2c6bca9c0d9c9ca213329a7215e8ecb8f0111781
Paludeto <gpalu54@gmail.com>
2025-05-26 19:15:02 -0300

    insercao com reuso

--- reuso/reuso.py
+++ reuso/reuso.py
@@ -29,6 +29,29 @@ def remove_registro(arquivo, chave):
     else:
         print("Chave não encontrada!")
 
+def insere_registro(arquivo, registro):
+
+    with open(arquivo, 'r', encoding='utf-8') as f:
+        linhas = f.readlines()
+
+    header = int(linhas[0].replace('\n', '').split(':')[1])
+    print(f"Topo da pilha antes: {header}")
+
+    if header == -1:
+        # Inserção no final do arquivo
+        linhas.append(registro + '\n')
+        print("Inserido no final do arquivo")
+    else:
+        # Inserção com reuso
+        pos = header + 1  # Ajuste porque header é o índice do nó anterior na pilha (posição -1 do índice da linha)
+        prox = int(linhas[pos].split('|')[1])  # Próximo topo da pilha
+        linhas[pos] = registro + '\n'
+        linhas[0] = f"head:{prox}\n"
+        print(f"Registro inserido no espaço liberado na linha {pos}")
+
+    with open(arquivo, 'w', encoding='utf-8') as f:
+        f.writelines(linhas)
+
 def compactacao_arquivo(arquivo):
 
     with open(arquivo, 'r', encoding='utf-8') as f:
@@ -45,4 +68,5 @@ if __name__ == '__main__':
 
     remove_registro('dataset.txt', 'Naruto Shippuuden')
     remove_registro('dataset.txt', 'Shugo Chara')
-    # compactacao_arquivo('dataset.txt')
+    insere_registro('dataset.txt','oi')
+    compactacao_arquivo('dataset.txt')
