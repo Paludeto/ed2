4261b2885b0517822c5f234a13fbfc8c84ec22b1
Paludeto <gpalu54@gmail.com>
2025-05-26 17:07:09 -0300

    remocao com pilha

--- reuso/reuso.py
+++ reuso/reuso.py
@@ -3,18 +3,31 @@ def remove_registro(arquivo, chave):
     with open(arquivo, 'r', encoding='utf-8') as f:
         linhas = f.readlines()
 
-    with open(arquivo, 'w', encoding='utf-8') as f:
-        found = False
-        for line in linhas:
-            nome = line.strip().split('|')[0]  # usando nome como key
-            if chave == nome:
-                novo_nome = nome.replace(nome[0:2], "*|")
-                line = line.replace(nome, novo_nome)
-                found = True
-                print("Registro nukado ☢️")
-            f.write(line)
-        if found == False:
-            print("Chave não encontrada!")
+    header = int(linhas[0].replace('\n', '').split(':')[1])
+    print(f"Topo da pilha antes: {header}")
+
+    found = False
+
+    for i in range(1, len(linhas)):
+        nome = linhas[i].strip().split('|')[0]
+
+        if chave == nome:
+            found = True
+
+            linhas[i] = f"*|{header}|{'|'.join(linhas[i].strip().split('|')[1:])}"
+            tam = 424 - len(linhas[i])
+            linhas[i] = linhas[i] + tam * '*' + '\n'
+            print("Registro nukado ☢️")
+
+            header = i - 1
+            break
+
+    if found:
+        linhas[0] = f"head:{header}\n"
+        with open(arquivo, 'w', encoding='utf-8') as f:
+            f.writelines(linhas)
+    else:
+        print("Chave não encontrada!")
 
 def compactacao_arquivo(arquivo):
 
@@ -29,7 +42,7 @@ def compactacao_arquivo(arquivo):
         f.writelines(linhas)
     
 if __name__ == '__main__':
-    
+
     remove_registro('dataset.txt', 'Naruto Shippuuden')
     remove_registro('dataset.txt', 'Shugo Chara')
-    compactacao_arquivo('dataset.txt')
+    # compactacao_arquivo('dataset.txt')
